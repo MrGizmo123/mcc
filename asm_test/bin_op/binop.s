6500ae49971cd9f993cb83943369af2150206b85
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2024-12-06 21:43:44 +0530

    add binary ops

---
+++ asm_test/bin_op/binop.s
@@ -0,0 +1,89 @@
+	;; program
+	lds 0xfffe           ; initialize stack pointer
+main:
+	;; Function prologue
+	ldrs %r15            ; move rsp to rbp
+	ldas                
+	ldbi 3              
+	sub                 
+	ldsa                
+
+	;; Add(Stack(0), Imm(10), Imm(9))
+
+	ldai 10              ; immediate
+	ldbi 9               ; immediate
+	add                 
+	ldra %r12            ; save the A register
+	ldrb %r13            ; save the B register
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 0               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmra %r12           ; load the value to the stack
+	ldar %r12            ; restore old values
+	ldbr %r13            ; restore old values
+
+
+	;; Sub(Stack(1), Imm(21), Stack(0))
+
+	ldai 21              ; immediate
+	ldra %r12            ; save the A register
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 0               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmaa                ; load the value from calculated address in stack frame
+	ldra %r13            ; auxiliary
+	ldbr %r13            ; transfer to B register
+	ldar %r12            ; put original value back in A register
+	sub                 
+	ldra %r12            ; save the A register
+	ldrb %r13            ; save the B register
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 1               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmra %r12           ; load the value to the stack
+	ldar %r12            ; restore old values
+	ldbr %r13            ; restore old values
+
+
+	;; BitAnd(Stack(2), Stack(1), Imm(6))
+
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 1               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmaa                ; load the value from calculated address in stack frame
+	ldbi 6               ; immediate
+	and                 
+	ldra %r12            ; save the A register
+	ldrb %r13            ; save the B register
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 2               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmra %r12           ; load the value to the stack
+	ldar %r12            ; restore old values
+	ldbr %r13            ; restore old values
+
+
+	;; Load(Reg(0), Stack(2))
+
+	;; Stack
+	ldar %r15            ; load rsp into A register
+	ldbi 2               ; load offset into B reg
+	sub                  ;  now address is stored in A reg, can be acessed using ldmaa
+
+	ldmaa                ; load the value from calculated address in stack frame
+	ldra %r0
+
+	;; Return()
+
+
+	hlt                  ; halt at the end of program
